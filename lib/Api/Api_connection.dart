d60a7ba4061c0f877bf5b67a4518e09dcede7969
Faixey <m2_folfol@hotmail.com>
2024-01-08 01:52:44 +0200

    commit

--- lib/Api/Api_connection.dart
+++ lib/Api/Api_connection.dart
@@ -1,5 +1,5 @@
 class API{
-  static const hostConnect="http://136.136.136.68/project_app";
+  static const hostConnect="http://192.168.1.168/project_app";
   static const hostConnectUser="$hostConnect/user";
   static const signUp = "$hostConnect/user/signup.php";
   static const validateEmail = "$hostConnect/user/validate_email.php";
